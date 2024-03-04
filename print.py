a760ecc2dc9bc39e4e824a47c5c999662fbdfae7
Dhanashri Gahiwal <gahiwald@gmail.com>
2024-03-04 16:14:18 +0530

    print() function

---
+++ print.py
@@ -0,0 +1,9 @@
+# print() function is used to print the output on the console.
+
+print("Hello World")
+print("Welcome to Python Programming", "Start learning...")
+print(5)
+print(2.3)
+print(2+3j)
+print(4+5.6)
+print("four:",4)
