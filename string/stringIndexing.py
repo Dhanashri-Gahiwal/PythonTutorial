5b8637b19a3ebf43f4821806e846fb6e98707d74
Dhanashri Gahiwal <gahiwald@gmail.com>
2024-03-06 18:21:47 +0530

    string indexing

---
+++ string/stringIndexing.py
@@ -0,0 +1,13 @@
+str = "Welcome to Python"
+print(str[0])
+print(str[4])
+print(str[-1])
+print(str[-5])
+
+'''
+from left to right index start from 0
+from right to left index start from -1
+-17|-16|-15|-14|-13|-12|-11|-10|-9|-8|-7|-6|-5|-4|-3|-2|-1 --> right to left index count
+W  |e  |l  |c  |o  |m  |e  |   |t |o |  |P |y |t |h |o |n
+0  |1  |2  |3  |4  |5  |6  |7  |8 |9 |10|11|12|13|14|15|16 --> left to right index count
+'''
