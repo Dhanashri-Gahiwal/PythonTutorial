8c86b104aa53ce8723d6b99a8188d9a1999a6037
Dhanashri Gahiwal <gahiwald@gmail.com>
2024-03-06 16:02:24 +0530

    input(), int(), float(), eval()

---
+++ userInput.py
@@ -0,0 +1,31 @@
+'''
+input() function is used to get the data from user
+by default input() returns the value in string
+'''
+
+# a = input("Enter a name")
+# print(a)
+
+# int() function is used to get the input in a integer format
+# num1 = int(input("Enter first num:"))
+# print(num1)
+# num2 = int(input("Enter second num:"))
+# print(num2)
+# add = num1 + num2
+# print(add)
+
+# float() function is used to get the input in a float format
+# num1 = float(input("Enter first num:"))
+# print(num1)
+# num2 = float(input("Enter second num:"))
+# print(num2)
+# add = num1 + num2
+# print(add)
+
+# eval() function is used to get the input in any format such as integer, float, binary
+num1 = eval(input("Enter first num:"))
+print(num1)
+num2 = eval(input("Enter second num:"))
+print(num2)
+add = num1 + num2
+print(add)
