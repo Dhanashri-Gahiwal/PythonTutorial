4374eb0c606b46b5cb37387d3139b5b00f0dfb4e
Dhanashri Gahiwal <gahiwald@gmail.com>
2024-07-05 14:03:07 +0530

    book store project in python-django with postgresql database

---
+++ django/myenv/books/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Book
+
+# Create your views here.
+def index(request):
+    # return HttpResponse("This is index page")
+    # return render(request,'index.html')
+    books = Book.objects
+    return render(request,'index.html',{'books':books})
