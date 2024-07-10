b0c2844671fc03070d552f928c43d346d920089b
Dhanashri Gahiwal <gahiwald@gmail.com>
2024-07-10 10:16:37 +0530

    extend html templates

--- python_django_projects/django1/cenv/contacts/views.py
+++ python_django_projects/django1/cenv/contacts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render,redirect
 from .models import Contact
 from contacts.forms import SaveContact
+from django.contrib import messages
 
 # Create your views here.
 def index(request):
@@ -9,12 +10,15 @@ def index(request):
         form = SaveContact(request.POST or None)
         if form.is_valid():
             form.save()
-    return render(request,'index.html',{'contacts':contacts})
+            messages.success(request,('Contact successfully added!'))
+            return render(request,'index.html',{'contacts':contacts})
+    else:    
+        return render(request,'index.html',{'contacts':contacts})
 
 def delete(request,contact_id):
     contact = Contact.objects.get(pk=contact_id)
     contact.delete()
-
+    messages.success(request,('Contact successfully deleted!'))
     return redirect('/')
 
 def edit(request,contact_id):
@@ -23,6 +27,7 @@ def edit(request,contact_id):
         form = SaveContact(request.POST or None,instance=contact)
         if form.is_valid():
             form.save()
+            messages.success(request,('Contact successfully updated!'))
         return redirect('/')
     else:
         contact = Contact.objects.get(pk=contact_id)
